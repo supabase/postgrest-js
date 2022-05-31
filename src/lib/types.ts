7850781727b4aea28fa22e17ca38d96e405edd0a
steinroe <philipp@steinroetter.com>
2022-05-31 15:52:25 +0200

    fix: pass allowEmpty in constructor

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -73,7 +73,7 @@ export abstract class PostgrestBuilder<T> implements PromiseLike<PostgrestRespon
     }
     this.fetch = (...args) => _fetch(...args)
     this.shouldThrowOnError = builder.shouldThrowOnError || false
-    this.allowEmpty = false
+    this.allowEmpty = builder.allowEmpty || false
   }
 
   /**
