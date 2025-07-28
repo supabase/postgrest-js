b1e1815e67ae31531ee486c6ef445f1a5c2d5745
avallete <andrew.valleteau@supabase.io>
2025-07-28 22:25:37 +0200

    chore(tests): add zod schema validation to ressource-embedding

--- test/resource-embedding.test.ts
+++ test/resource-embedding.test.ts
@@ -2,6 +2,7 @@ import { PostgrestClient } from '../src/index'
 import { Database } from './types.override'
 import { expectType } from 'tsd'
 import { TypeEqual } from 'ts-expect'
+import { z } from 'zod'
 
 const postgrest = new PostgrestClient<Database>('http://localhost:3000')
 
@@ -56,16 +57,23 @@ test('embedded select', async () => {
     }
   `)
   let result: Exclude<typeof res.data, null>
-  let expected: {
-    messages: {
-      channel_id: number
-      data: unknown
-      id: number
-      message: string | null
-      username: string
-    }[]
-  }[]
+  const ExpectedSchema = z.array(
+    z.object({
+      messages: z.array(
+        z.object({
+          channel_id: z.number(),
+          data: z.unknown().nullable(),
+          id: z.number(),
+          message: z.string().nullable(),
+          username: z.string(),
+        })
+      ),
+    })
+  )
+  type ExpectedType = z.infer<typeof ExpectedSchema>
+  let expected: Array<{ messages: Array<Required<ExpectedType[number]['messages'][number]>> }>
   expectType<TypeEqual<typeof result, typeof expected>>(true)
+  ExpectedSchema.parse(res.data)
 })
 
 test('embedded select with computed field explicit selection', async () => {
@@ -122,17 +130,24 @@ test('embedded select with computed field explicit selection', async () => {
     }
   `)
   let result: Exclude<typeof res.data, null>
-  let expected: {
-    messages: {
-      channel_id: number
-      data: unknown
-      id: number
-      message: string | null
-      username: string
-      blurb_message: string | null
-    }[]
-  }[]
+  const ExpectedSchema = z.array(
+    z.object({
+      messages: z.array(
+        z.object({
+          channel_id: z.number(),
+          data: z.unknown().nullable(),
+          id: z.number(),
+          message: z.string().nullable(),
+          username: z.string(),
+          blurb_message: z.string().nullable(),
+        })
+      ),
+    })
+  )
+  type ExpectedType = z.infer<typeof ExpectedSchema>
+  let expected: Array<{ messages: Array<Required<ExpectedType[number]['messages'][number]>> }>
   expectType<TypeEqual<typeof result, typeof expected>>(true)
+  ExpectedSchema.parse(res.data)
 })
 
 describe('embedded filters', () => {
@@ -176,16 +191,23 @@ describe('embedded filters', () => {
       }
     `)
     let result: Exclude<typeof res.data, null>
-    let expected: {
-      messages: {
-        channel_id: number
-        data: unknown
-        id: number
-        message: string | null
-        username: string
-      }[]
-    }[]
+    const ExpectedSchema = z.array(
+      z.object({
+        messages: z.array(
+          z.object({
+            channel_id: z.number(),
+            data: z.unknown().nullable(),
+            id: z.number(),
+            message: z.string().nullable(),
+            username: z.string(),
+          })
+        ),
+      })
+    )
+    type ExpectedType = z.infer<typeof ExpectedSchema>
+    let expected: Array<{ messages: Array<Required<ExpectedType[number]['messages'][number]>> }>
     expectType<TypeEqual<typeof result, typeof expected>>(true)
+    ExpectedSchema.parse(res.data)
   })
   test('embedded or', async () => {
     const res = await postgrest
@@ -233,16 +255,23 @@ describe('embedded filters', () => {
       }
     `)
     let result: Exclude<typeof res.data, null>
-    let expected: {
-      messages: {
-        channel_id: number
-        data: unknown
-        id: number
-        message: string | null
-        username: string
-      }[]
-    }[]
+    const ExpectedSchema = z.array(
+      z.object({
+        messages: z.array(
+          z.object({
+            channel_id: z.number(),
+            data: z.unknown().nullable(),
+            id: z.number(),
+            message: z.string().nullable(),
+            username: z.string(),
+          })
+        ),
+      })
+    )
+    type ExpectedType = z.infer<typeof ExpectedSchema>
+    let expected: Array<{ messages: Array<Required<ExpectedType[number]['messages'][number]>> }>
     expectType<TypeEqual<typeof result, typeof expected>>(true)
+    ExpectedSchema.parse(res.data)
   })
   test('embedded or with and', async () => {
     const res = await postgrest
@@ -292,16 +321,23 @@ describe('embedded filters', () => {
       }
     `)
     let result: Exclude<typeof res.data, null>
-    let expected: {
-      messages: {
-        channel_id: number
-        data: unknown
-        id: number
-        message: string | null
-        username: string
-      }[]
-    }[]
+    const ExpectedSchema = z.array(
+      z.object({
+        messages: z.array(
+          z.object({
+            channel_id: z.number(),
+            data: z.unknown().nullable(),
+            id: z.number(),
+            message: z.string().nullable(),
+            username: z.string(),
+          })
+        ),
+      })
+    )
+    type ExpectedType = z.infer<typeof ExpectedSchema>
+    let expected: Array<{ messages: Array<Required<ExpectedType[number]['messages'][number]>> }>
     expectType<TypeEqual<typeof result, typeof expected>>(true)
+    ExpectedSchema.parse(res.data)
   })
 })
 
@@ -359,16 +395,23 @@ describe('embedded transforms', () => {
       }
     `)
     let result: Exclude<typeof res.data, null>
-    let expected: {
-      messages: {
-        channel_id: number
-        data: unknown
-        id: number
-        message: string | null
-        username: string
-      }[]
-    }[]
+    const ExpectedSchema = z.array(
+      z.object({
+        messages: z.array(
+          z.object({
+            channel_id: z.number(),
+            data: z.unknown().nullable(),
+            id: z.number(),
+            message: z.string().nullable(),
+            username: z.string(),
+          })
+        ),
+      })
+    )
+    type ExpectedType = z.infer<typeof ExpectedSchema>
+    let expected: Array<{ messages: Array<Required<ExpectedType[number]['messages'][number]>> }>
     expectType<TypeEqual<typeof result, typeof expected>>(true)
+    ExpectedSchema.parse(res.data)
   })
 
   test('embedded order on multiple columns', async () => {
@@ -425,16 +468,23 @@ describe('embedded transforms', () => {
       }
     `)
     let result: Exclude<typeof res.data, null>
-    let expected: {
-      messages: {
-        channel_id: number
-        data: unknown
-        id: number
-        message: string | null
-        username: string
-      }[]
-    }[]
+    const ExpectedSchema = z.array(
+      z.object({
+        messages: z.array(
+          z.object({
+            channel_id: z.number(),
+            data: z.unknown().nullable(),
+            id: z.number(),
+            message: z.string().nullable(),
+            username: z.string(),
+          })
+        ),
+      })
+    )
+    type ExpectedType = z.infer<typeof ExpectedSchema>
+    let expected: Array<{ messages: Array<Required<ExpectedType[number]['messages'][number]>> }>
     expectType<TypeEqual<typeof result, typeof expected>>(true)
+    ExpectedSchema.parse(res.data)
   })
 
   test('embedded limit', async () => {
@@ -476,16 +526,23 @@ describe('embedded transforms', () => {
       }
     `)
     let result: Exclude<typeof res.data, null>
-    let expected: {
-      messages: {
-        channel_id: number
-        data: unknown
-        id: number
-        message: string | null
-        username: string
-      }[]
-    }[]
+    const ExpectedSchema = z.array(
+      z.object({
+        messages: z.array(
+          z.object({
+            channel_id: z.number(),
+            data: z.unknown().nullable(),
+            id: z.number(),
+            message: z.string().nullable(),
+            username: z.string(),
+          })
+        ),
+      })
+    )
+    type ExpectedType = z.infer<typeof ExpectedSchema>
+    let expected: Array<{ messages: Array<Required<ExpectedType[number]['messages'][number]>> }>
     expectType<TypeEqual<typeof result, typeof expected>>(true)
+    ExpectedSchema.parse(res.data)
   })
 
   test('embedded range', async () => {
@@ -527,15 +584,22 @@ describe('embedded transforms', () => {
       }
     `)
     let result: Exclude<typeof res.data, null>
-    let expected: {
-      messages: {
-        channel_id: number
-        data: unknown
-        id: number
-        message: string | null
-        username: string
-      }[]
-    }[]
+    const ExpectedSchema = z.array(
+      z.object({
+        messages: z.array(
+          z.object({
+            channel_id: z.number(),
+            data: z.unknown().nullable(),
+            id: z.number(),
+            message: z.string().nullable(),
+            username: z.string(),
+          })
+        ),
+      })
+    )
+    type ExpectedType = z.infer<typeof ExpectedSchema>
+    let expected: Array<{ messages: Array<Required<ExpectedType[number]['messages'][number]>> }>
     expectType<TypeEqual<typeof result, typeof expected>>(true)
+    ExpectedSchema.parse(res.data)
   })
 })
